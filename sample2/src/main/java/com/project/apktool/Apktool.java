24d1f8ca57ec2a50b3cecee9373b1cc79aa007d0
msudesh_kumar <msudesh_kumar@yahoo.co.in>
2017-04-08 01:26:27 +0530

    Commit - 2

--- sample2/src/main/java/com/project/apktool/Apktool.java
+++ sample2/src/main/java/com/project/apktool/Apktool.java
@@ -6,10 +6,10 @@ import com.project.utils.Str_Int;
 public class Apktool {
 	public static void ApktoolExec(String apkfilepath)
 	{
-		String ipfile = apkfilepath;
+		String ipfile = apkfilepath;			
+		LinuxInteractor li = new LinuxInteractor();
 		try
 		{
-			LinuxInteractor li = new LinuxInteractor();
 			Str_Int op = li.executeCommand("/usr/bin/java -jar /opt/apktool_2.2.2.jar/apktool_2.2.2.jar d "+ipfile+" -o \"/tmp/op\" -f",true);
 			System.out.println("Response: "+op.get_respstring()+"Exit Status: "+op.get_exitstat());
 		}
