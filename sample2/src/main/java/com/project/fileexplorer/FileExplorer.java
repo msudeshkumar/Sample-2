2c59d2caff11a5e59514bfd5d14fcb98e8f5bcbb
msudesh_kumar <msudesh_kumar@yahoo.co.in>
2017-04-09 00:32:40 +0530

    Commit - 3

--- sample2/src/main/java/com/project/fileexplorer/FileExplorer.java
+++ sample2/src/main/java/com/project/fileexplorer/FileExplorer.java
@@ -1,5 +1,52 @@
 package com.project.fileexplorer;
 
-public class FileExplorer {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
+public class FileExplorer {
+	static ArrayList<String> l1 = new ArrayList<String>();
+	public static void explorer()
+	{
+		try
+		{	
+			String filename = "/tmp/op/smali";
+			BufferedWriter op = new BufferedWriter(new FileWriter("/tmp/op/smalilist.txt"));
+			searcher(filename);
+			for(int j=0;j<l1.size();j++)
+			{
+				op.write(l1.get(j).toString());
+				op.newLine();
+			}
+			op.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void searcher(String folderarg)
+	{
+		try
+		{ 
+			File folder = new File(folderarg);
+			File[] listOfFiles = folder.listFiles();
+		    	for (int i = 0; i < listOfFiles.length; i++) 
+		    	{
+		    		if (listOfFiles[i].isFile()) 
+		    		{
+		    			l1.add(listOfFiles[i].getPath().toString()+listOfFiles[i].getName().toString());
+		    		} 
+		    		else if (listOfFiles[i].isDirectory()) 
+		    		{
+		    			searcher(listOfFiles[i].toString());
+		    		}
+		    	}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
