d8cc32d583fca5287315fd22385716913fd0408b
root <root@sudesh-Inspiron-One-2320>
2017-05-07 18:34:03 +0530

    Commit-1

---
+++ DBNTest/DBNTest/src/com/project/deeplearningengine/DLTest.java
@@ -0,0 +1,35 @@
+package com.project.deeplearningengine;
+import com.project.deeplearningengine.DBN;;
+public class DLTest {
+	public static void main(String[] args) {
+		int[][] train_X = {
+         		{0,0,0,1,0,1},
+                 {0,0,0,1,0,1},
+                 {0,0,0,1,0,1},
+                 {0,0,0,1,0,1},
+                 {0,0,0,1,0,1},
+                 {0,0,0,1,0,1},
+                 {1,1,1,1,0,0},
+                 {1,0,1,1,0,1},
+                 {0,0,0,1,0,1}
+         };
+
+         int[][] train_Y = {
+         		 {1, 0, 1, 0, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 0, 0, 1, 1},
+                  {1, 0, 1, 1, 1},
+                  {1, 0, 1, 1, 1},
+                  {0, 1, 1, 1, 0},
+                  {0, 1, 1, 1, 0},
+                  {1, 0, 0, 1, 0}
+         };
+         int[][] test_X = {
+         		{1,1,1,1,0,0},
+         		{1,0,0,1,0,1}
+         };
+         DBN.deepEngine(train_X, train_Y, test_X);
+	}
+
+}
