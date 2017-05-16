509f3bf15f26e62f7862a31bd7d66b1205648469
root <root@sudesh-Inspiron-One-2320>
2017-05-16 08:31:43 +0530

    Commit From Home - 5

---
+++ sample2/src/main/java/com/project/featureextraction/APIFeatureExtractor.java
@@ -0,0 +1,95 @@
+package com.project.featureextraction;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class APIFeatureExtractor {
+	public static void apiFeatureExtractor(List filepath) throws IOException
+	{
+		List fpaths = filepath;
+		String X;
+		String feac = null;
+		int[] fea = {0,0,0,0,0,0};
+		BufferedWriter op = null;
+		int flag1 =0, flag2 =0;
+		for(int i=0;i<filepath.getItemCount();i++)
+		{
+			FileReader fr = new FileReader(fpaths.getItem(i)+"/smali/func.txt");
+			BufferedReader br = new BufferedReader(fr);
+			FileWriter fw = new FileWriter(fpaths.getItem(i)+"/APIFeatures.txt");
+			//System.out.println(fpaths.getItem(i)+"/APIFeatures.txt");
+			op = new BufferedWriter(fw);
+			while((X=br.readLine())!=null)
+			{
+				//System.out.println(X);
+				//System.out.println("#"+X.trim().substring(0, X.indexOf(")")+1));
+				if(X.isEmpty())
+				 {
+					//System.out.println("Is Empty");
+					 continue;
+				 }
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/content/Context;->getApplicationContext()")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/content/Context;->getApplicationContext()");
+					//flag1 = 1;
+					fea[0] = 1;
+				}
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/content/Intent;->getAction()")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/content/Intent;->getAction()");
+					//flag2 = 1;
+					fea[1] = 1;
+				}
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/content/DialogInterface;->dismiss()")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/content/DialogInterface;->dismiss()");
+					//flag2 = 1;
+					fea[2] = 1;
+				}
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/view/MotionEvent;->getPointerCount()")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/view/MotionEvent;->getPointerCount()");
+					//flag2 = 1;
+					fea[3] = 1;
+				}
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/app/AlertDialog;->isShowing()")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/app/AlertDialog;->isShowing()");
+					//flag2 = 1;
+					fea[4] = 1;
+				}
+				if((X.trim().substring(0, X.indexOf(")")+1)).compareTo("Landroid/os/CountDownTimer;-><init>(JJ)")==0)
+				{
+					//System.out.println("**"+X.trim().substring(0, X.indexOf(")"))+":"+"Landroid/app/AlertDialog;->isShowing()");
+					//flag2 = 1;
+					fea[5] = 1;
+				}
+			}
+			feac = Arrays.toString(fea);
+			System.out.println(feac);
+			op.write(feac);
+			//op.newLine();
+			op.close();
+		}
+		//feac = fea.toString();
+		
+		
+	}
+	public static void main(String[] Args) throws IOException
+	{
+		List l1 = new List();
+		l1.add("/tmp/op/file0");
+		l1.add("/tmp/op/file1");
+		l1.add("/tmp/op/file2");
+		//l1.add("/tmp/op/file3/smali/smalilist.txt/");
+		apiFeatureExtractor(l1);
+	}
+
+}
